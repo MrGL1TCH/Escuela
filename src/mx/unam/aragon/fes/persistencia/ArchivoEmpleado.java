7cd4a1493a38d234a87ddb17d2448a269a532365
GL1TCH <halo_nenito16@hotmail.com>
2021-03-08 17:24:20 -0600

    Cuestionario 6

---
+++ src/mx/unam/aragon/fes/persistencia/ArchivoEmpleado.java
@@ -0,0 +1,36 @@
+package mx.unam.aragon.fes.persistencia;
+
+import java.util.ArrayList;
+import mx.unam.aragon.fes.Empleado;
+
+
+public class ArchivoEmpleado {
+    private String archivo;
+
+    public ArchivoEmpleado() {
+    }
+
+    public ArchivoEmpleado(String archivo) {
+        this.archivo = archivo;
+    }
+
+    public String getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(String archivo) {
+        this.archivo = archivo;
+    }
+    
+    public void guardarEmpleados(ArrayList<Empleado>datos){
+        //aqui el codigo para grabar en Disco Duro
+    }
+    
+    public ArrayList<Empleado> leerEmpleados(){
+        ArrayList<Empleado> tmp=null;
+        
+        //leer del disco duro
+        //se sube a un ArrayList tmp
+        return tmp;
+    }
+}
